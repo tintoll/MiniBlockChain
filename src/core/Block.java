e2d0bbb06f4fcdf69ad91c7a6fa03567ebeca63c
tintoll <belselios@gmail.com>
2019-03-18 08:26:09 +0900

    chap 5

--- src/core/Block.java
+++ src/core/Block.java
@@ -2,12 +2,14 @@ package core;
 
 import util.Util;
 
+import java.util.List;
+
 public class Block {
 
     private int blockID;
     private String previousBlockHash;
     private int nonce;
-    private String data;
+    private List<Transaction> transactionList;
 
     public int getBlockID() {
         return blockID;
@@ -25,13 +27,6 @@ public class Block {
         this.nonce = nonce;
     }
 
-    public String getData() {
-        return data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
-    }
 
     public String getPreviousBlockHash() {
         return previousBlockHash;
@@ -41,25 +36,36 @@ public class Block {
         this.previousBlockHash = previousBlockHash;
     }
 
-    public Block(int blockID, String previousBlockHash, int nonce, String data) {
+    public Block(int blockID, String previousBlockHash, int nonce, List<Transaction> transactionList) {
         this.blockID = blockID;
         this.previousBlockHash = previousBlockHash;
         this.nonce = nonce;
-        this.data = data;
+        this.transactionList = transactionList;
     }
 
-    public void getInformation() {
+    public void addTransaction(Transaction transaction) {
+        transactionList.add(transaction);
+    }
+
+    public void showInformation() {
         System.out.println("--------------------");
         System.out.println("블록 번호: " + getBlockID());
         System.out.println("이전 해시: " + getPreviousBlockHash());
         System.out.println("채굴 변수 값: " + getNonce());
-        System.out.println("블록 데이터: " + getData());
+        System.out.println("트랜잭션 개수 : " + transactionList.size()+"개");
+        for(int i = 0; i < transactionList.size(); i++) {
+                System.out.println(transactionList.get(i).getInformation());
+        }
         System.out.println("블록 해시: " + getBlockHash());
         System.out.println("--------------------");
     }
 
     public String getBlockHash() {
-        return Util.getHash(nonce + data + previousBlockHash);
+        String transactionInformations = "";
+        for(int i = 0; i < transactionList.size(); i++) {
+            transactionInformations += transactionList.get(i).getInformation();
+        }
+        return Util.getHash(nonce + transactionInformations + previousBlockHash);
     }
 
     public void mine() {
--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -2,24 +2,31 @@ package core;
 
 import util.Util;
 
+import java.util.ArrayList;
+
 public class BlockChainStarter {
 
 
     public static void main(String[] args) {
-        Block block1 = new Block(1, null, 0, "데이터");
+        Block block1 = new Block(1, null, 0, new ArrayList<>());
         block1.mine();
-        block1.getInformation();
+        block1.showInformation();
 
-        Block block2 = new Block(2, block1.getBlockHash(), 0, "데이터");
+        Block block2 = new Block(2, block1.getBlockHash(), 0, new ArrayList<>());
+        block2.addTransaction(new Transaction("홍1", "홍2", 1.5));
+        block2.addTransaction(new Transaction("홍3", "홍2", 0.7));
         block2.mine();
-        block2.getInformation();
+        block2.showInformation();
 
-        Block block3 = new Block(3, block2.getBlockHash(), 0, "데이터");
+        Block block3 = new Block(3, block2.getBlockHash(), 0, new ArrayList<>());
+        block3.addTransaction(new Transaction("홍4", "홍5", 8.2));
+        block3.addTransaction(new Transaction("홍6", "홍1", 0.4));
         block3.mine();
-        block3.getInformation();
+        block3.showInformation();
 
-        Block block4 = new Block(4, block3.getBlockHash(), 0, "데이터");
+        Block block4 = new Block(4, block3.getBlockHash(), 0, new ArrayList<>());
+        block4.addTransaction(new Transaction("홍4", "홍6", 0.1));
         block4.mine();
-        block4.getInformation();
+        block4.showInformation();
     }
 }
