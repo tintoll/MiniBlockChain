d83a710da27f2bf9fc8dd69e8cdfebff288d1ed7
tintoll <belselios@gmail.com>
2019-03-18 08:13:35 +0900

    chap 4

--- src/core/BlockChainStarter.java
+++ src/core/BlockChainStarter.java
@@ -6,13 +6,20 @@ public class BlockChainStarter {
 
 
     public static void main(String[] args) {
-        int nonce = 0;
-        while(true) {
-            if(Util.getHash(nonce+"").substring(0,6).equals("000000")) {
-                System.out.println("정답 : "+ nonce);
-                break;
-            }
-            nonce++;
-        }
+        Block block1 = new Block(1, null, 0, "데이터");
+        block1.mine();
+        block1.getInformation();
+
+        Block block2 = new Block(2, block1.getBlockHash(), 0, "데이터");
+        block2.mine();
+        block2.getInformation();
+
+        Block block3 = new Block(3, block2.getBlockHash(), 0, "데이터");
+        block3.mine();
+        block3.getInformation();
+
+        Block block4 = new Block(4, block3.getBlockHash(), 0, "데이터");
+        block4.mine();
+        block4.getInformation();
     }
 }
